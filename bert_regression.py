f118bf6762d76f4b23f08c2379f36cc499581019
Bayu Wicaksono <bwbayuuu@gmail.com>
2025-01-01 15:50:11 +0700

    feat/add custom bert model for regression

---
+++ bert_regression.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+from transformers import BertModel, AutoModel
+
+class BertRegressionModel(nn.Module):
+    def __init__(self, bert_model_name='bert-base-uncased'):
+        super().__init__()
+        # load pretrained model
+        self.bert = AutoModel.from_pretrained(bert_model_name)
+        # add regression layer
+        self.regression_layer = nn.Linear(self.bert.config.hidden_size, 1) # 768 x 1 -> output layer regression
+
+    def forward(self, input_ids, attention_mask=None, token_type_ids=None):
+        outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
+        # print("output:", outputs)
+        # print("last hidden state:", outputs.last_hidden_state)
+        cls_embedding = outputs.last_hidden_state[:, 0, :]
+        # print("cls embedding", cls_embedding)
+        score = self.regression_layer(cls_embedding)
+        return score
